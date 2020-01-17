03362b7f10e15f5a5c163d7e2575abd091dc18c7
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-17 02:41:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,35 @@
 def create_an_empty_array
-  
+  array = []
+  return array
 end
 
 def create_an_array
-  
+  array = [1,1,1,1]
+  return array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
+  return array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
+  return array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
+  return array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift()
+  return array
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
