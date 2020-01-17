0c18bfe79bb2932de4d12c1432478dba43d8f26e
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-17 04:10:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return_element = array[array.length - 1]
+  return return_element
 end
 
 def update_element_from_index(array, index_number, element)
